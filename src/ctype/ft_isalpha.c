1eaacc73a782b1eb5301169221b1945806b1e41d
Caruychen <caruychen@gmail.com>
2022-01-01 22:56:51 +0200

    clean ctype function

--- src/ctype/ft_isalpha.c
+++ src/ctype/ft_isalpha.c
@@ -6,23 +6,13 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 11:34:41 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/11 14:55:45 by cchen            ###   ########.fr       */
+/*   Updated: 2022/01/01 22:55:07 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	is_upper(int c)
-{
-	return (c >= 'A' && c <= 'Z');
-}
-
-static int	is_lower(int c)
-{
-	return (c >= 'a' && c <= 'z');
-}
-
 int	ft_isalpha(int c)
 {
-	return (is_upper(c) || is_lower(c));
+	return (ft_isupper(c) || ft_islower(c));
 }
