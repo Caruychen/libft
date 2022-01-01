2a599ef4aff904eecf5a46fba754d9a682c6377c
Caruychen <caruychen@gmail.com>
2022-01-01 23:41:32 +0200

    added ft_stdlib.h

---
+++ includes/libft/ft_stdlib.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stdlib.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/01 23:35:07 by cchen             #+#    #+#             */
+/*   Updated: 2022/01/01 23:37:42 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STDLIB_H
+# define FT_STDLIB_H
+
+int		ft_atoi(const char *str);
+void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
+char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
+char	*ft_itoa(int n);
+void	ft_memdelarray(void ***arr);
+void	ft_strdelarray(char ***arr);
+
+#endif
