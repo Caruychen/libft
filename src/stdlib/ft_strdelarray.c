058c551d54a2f3c9a5b1e53db79f00bb40c1535f
Caruy Chen <cchen@student.hive.fi>
2022-01-03 12:09:20 +0200

    fix typo

--- src/stdlib/ft_strdelarray.c
+++ src/stdlib/ft_strdelarray.c
@@ -6,12 +6,12 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 09:18:19 by cchen             #+#    #+#             */
-/*   Updated: 2022/01/01 23:33:01 by cchen            ###   ########.fr       */
+/*   Updated: 2022/01/03 12:08:42 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include "ft_stdlib.h>
+#include "ft_stdlib.h"
 
 void	ft_strdelarray(char ***arr)
 {
