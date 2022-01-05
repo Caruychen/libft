ce9d1fedc91e615d9a358533dc314c5614fda94c
Caruy Chen <cchen@student.hive.fi>
2022-01-05 15:39:39 +0200

    rename vec functions

--- includes/libft/vec.h
+++ includes/libft/vec.h
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 11:01:19 by cchen             #+#    #+#             */
-/*   Updated: 2021/12/21 11:36:13 by cchen            ###   ########.fr       */
+/*   Updated: 2022/01/05 15:39:32 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ typedef struct s_vec
 	size_t			len;
 }		t_vec;
 
-int		ft_vecnew(t_vec *dst, size_t init_alloc, size_t elem_size);
-void	ft_vecfree(t_vec *src);
-int		ft_vecfrom(t_vec *dst, void *src, size_t len, size_t elem_size);
-int		ft_veccopy(t_vec *dst, t_vec *src);
-int		ft_vecresize(t_vec *src, size_t target_size);
+int		vec_new(t_vec *dst, size_t init_alloc, size_t elem_size);
+void	vec_free(t_vec *src);
+int		vec_from(t_vec *dst, void *src, size_t len, size_t elem_size);
+int		vec_copy(t_vec *dst, t_vec *src);
+int		vec_resize(t_vec *src, size_t target_size);
 
 #endif
