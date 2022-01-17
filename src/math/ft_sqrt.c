1789e839bdfa9f6afd2048d215078186b518e6b4
Caruy Chen <cchen@student.hive.fi>
2022-01-17 16:04:39 +0200

    added ft_sqrt to math lib

--- src/math/ft_sqrt.c
+++ src/math/ft_sqrt.c
@@ -1,19 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_math.h                                          :+:      :+:    :+:   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/01 23:40:35 by cchen             #+#    #+#             */
-/*   Updated: 2022/01/17 13:34:48 by cchen            ###   ########.fr       */
+/*   Created: 2022/01/17 13:30:05 by cchen             #+#    #+#             */
+/*   Updated: 2022/01/17 13:34:07 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_MATH_H
-# define FT_MATH_H
+int	ft_sqrt(int nb)
+{
+	long int	start;
+	long int	end;
+	long int	mid;
 
-int	ft_imin(const int n1, const int n2);
-int	ft_sqrt(int nb);
-
-#endif
+	start = 0;
+	end = nb;
+	while (start <= end)
+	{
+		mid = (start + end) / 2;
+		if (mid * mid == nb)
+			return (mid);
+		else  if (mid * mid  < nb)
+			start = mid + 1;
+		else
+			end = mid - 1;
+	}
+	return (0);
+}
