20592d166ec20fb3b47d75092bb17ec0c81560ca
Caruychen <caruychen@gmail.com>
2022-01-22 18:56:44 +0200

    improve gnl efficiency

--- includes/libft/get_next_line.h
+++ includes/libft/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 12:54:33 by cchen             #+#    #+#             */
-/*   Updated: 2022/01/06 14:44:48 by cchen            ###   ########.fr       */
+/*   Updated: 2022/01/22 18:53:14 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 256
 # define FD_MAX 8192
 # define RESIZE_FACTOR 2
 
--- src/stdio/get_next_line.c
+++ src/stdio/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 11:45:43 by cchen             #+#    #+#             */
-/*   Updated: 2022/01/01 14:06:34 by cchen            ###   ########.fr       */
+/*   Updated: 2022/01/22 18:30:31 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,58 +23,60 @@ static void	set_buff(t_vec *buff, size_t index)
 	ft_memcpy(buff->memory, (char *)buff->memory + index + 1, buff->len);
 }
 
-static int	push_line(t_vec *buff, char **line)
+static int	push_line(t_vec *buff, char **line, size_t len)
 {
-	size_t	index;
-
-	index = 0;
-	while (((char *)buff->memory)[index] != '\n' && index < buff->len)
-		++index;
-	*line = ft_strsub(buff->memory, 0, index);
+	*line = ft_strsub(buff->memory, 0, len);
 	if (!*line)
 	{
 		vec_free(buff);
 		return (-1);
 	}
-	set_buff(buff, index);
+	set_buff(buff, len);
 	return (1);
 }
 
-static long	result(t_vec *buff, char **line, long bytes)
+static long	result(t_vec *buff, char **line, long res, char *nl)
 {
-	if (bytes < 0 || (bytes == 0 && !buff->len))
+	if (res < 0 || (res == 0 && !buff->len))
 	{
 		vec_free(buff);
-		return (bytes);
+		return (res);
 	}
-	return (push_line(buff, line));
+	if (nl)
+		return (push_line(buff, line, (size_t)(nl - (char *)buff->memory)));
+	return (push_line(buff, line, buff->len));
 }
 
-static long	read_line(int fd, t_vec *buff, long *bytes)
+static long	read_line(int fd, t_vec *buff, long *res)
 {
 	if (buff->alloc_size - buff->len < BUFF_SIZE
 		&& vec_resize(buff, buff->alloc_size * RESIZE_FACTOR) == -1)
-		return (*bytes = -1);
-	return (*bytes = read(fd, (char *)buff->memory + buff->len, BUFF_SIZE));
+		return (*res = -1);
+	return (*res = read(fd, (char *)buff->memory + buff->len, BUFF_SIZE));
 }
 
 int	get_next_line(const int fd, char **line)
 {
 	static t_vec	vectors[FD_MAX];
 	t_vec			*buff;
-	long			bytes;
+	char			*nl;
+	long			res;
 
 	if (fd < 0 || fd >= FD_MAX || !line)
 		return (-1);
 	buff = &vectors[fd];
 	if (!buff->memory && vec_new(buff, BUFF_SIZE, sizeof(char)) == -1)
 		return (-1);
-	while (read_line(fd, buff, &bytes) > 0)
+	nl = (char *)ft_memchr((char *)buff->memory, '\n', (size_t) buff->len);
+	if (nl)
+		return ((int) result(buff, line, 1, nl));
+	while (read_line(fd, buff, &res) > 0)
 	{
-		buff->len += (size_t) bytes;
-		if (ft_memchr((char *)buff->memory + buff->len - bytes, '\n',
-				(size_t) bytes))
+		buff->len += (size_t) res;
+		nl = ft_memchr((char *)buff->memory + buff->len - res, '\n',
+				(size_t) res);
+		if (nl)
 			break ;
 	}
-	return ((int) result(buff, line, bytes));
+	return ((int) result(buff, line, res, nl));
 }
