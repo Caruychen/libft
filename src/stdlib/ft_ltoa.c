5f6d8a858e7e6a0dcaf95030ebd1463109ce0fcb
Caruy Chen <cchen@student.hive.fi>
2022-02-28 11:21:33 +0200

    fix int negative limit edge case

--- src/stdlib/ft_ltoa.c
+++ src/stdlib/ft_ltoa.c
@@ -6,25 +6,29 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 17:11:10 by cchen             #+#    #+#             */
-/*   Updated: 2022/02/18 11:19:10 by cchen            ###   ########.fr       */
+/*   Updated: 2022/02/28 11:21:15 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdint.h>
+#include <limits.h>
 #include "ft_string.h"
 #include "ft_stdlib.h"
 
-static	int	set_sign(intmax_t *value)
+static int	_internal_ultoa(uintmax_t value, char *tmp, int base)
 {
-	int	sign;
+	int	index;
 
-	sign = 1;
-	if (*value < 0)
+	index = MAX_LL_STR_SIZE - 1;
+	tmp[index--] = '\0';
+	while (value > 0)
 	{
-		sign = -1;
-		*value *= -1;
+		tmp[index--] = HEX_STR_L[value % base];
+		value /= base;
 	}
-	return (sign);
+	if (index == MAX_LL_STR_SIZE - 2)
+		tmp[index--] = '0';
+	return (index);
 }
 
 char	*ft_ltoa(intmax_t value, int base)
@@ -36,18 +40,10 @@ char	*ft_ltoa(intmax_t value, int base)
 
 	if (base < 2 || base > 16)
 		return (NULL);
-	sign = set_sign(&value);
-	index = MAX_LL_STR_SIZE - 1;
-	tmp[index--] = '\0';
-	while (value > 0)
-	{
-		tmp[index--] = HEX_STR_L[value % base];
-		value /= base;
-	}
+	sign = 1 - ((value < 0) * 2);
+	index = _internal_ultoa(value, tmp, base);
 	if (sign == -1 && base == 10)
 		tmp[index--] = '-';
-	if (index == MAX_LL_STR_SIZE - 2)
-		tmp[index--] = '0';
 	res = (char *)malloc(MAX_LL_STR_SIZE - index + 1);
 	if (!res)
 		return (NULL);
