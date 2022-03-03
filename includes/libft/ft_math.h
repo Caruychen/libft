6d1e5c897af1cd1b84b8473601b77e30ece8890c
Caruy Chen <cchen@student.hive.fi>
2022-03-03 14:22:02 +0200

    add missing parts to ft_math

--- includes/libft/ft_math.h
+++ includes/libft/ft_math.h
@@ -6,12 +6,20 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 23:40:35 by cchen             #+#    #+#             */
-/*   Updated: 2022/03/03 14:20:34 by cchen            ###   ########.fr       */
+/*   Updated: 2022/03/03 14:21:43 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MATH_H
 # define FT_MATH_H
+
+# include <stdint.h>
+
+typedef union s_dcast
+{
+	double		d;
+	uint64_t	u;
+}			t_dcast;
 
 int	ft_imin(const int n1, const int n2);
 int	ft_sqrt(int nb);
