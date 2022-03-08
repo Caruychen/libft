8ba05cf128e5b3d1af35c4e7ea9f8cc52bdb9ae5
Caruy Chen <cchen@student.hive.fi>
2022-03-08 10:29:53 +0200

    added ft_imax

--- src/math/ft_imax.c
+++ src/math/ft_imax.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_math.h                                          :+:      :+:    :+:   */
+/*   ft_imax.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/01 23:40:35 by cchen             #+#    #+#             */
-/*   Updated: 2022/03/08 10:26:52 by cchen            ###   ########.fr       */
+/*   Created: 2022/03/08 10:25:21 by cchen             #+#    #+#             */
+/*   Updated: 2022/03/08 10:25:57 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_MATH_H
-# define FT_MATH_H
-
-# include <stdint.h>
-
-typedef union s_dcast
+int	ft_imax(const int n1, const int n2)
 {
-	double		d;
-	uint64_t	u;
-}			t_dcast;
-
-int			ft_imin(const int n1, const int n2);
-int			ft_imax(const int n1, const int n2);
-int			ft_sqrt(int nb);
-double		ft_fmod(double x, double y);
-long long	ft_pow(long long x, long long y);
-
-#endif
+	if (n1 < n2)
+		return (n2);
+	return (n1);
+}
