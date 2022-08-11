467c89212e3fb434eb91f84a7d058bd8a6b0f22d
Caruy Chen <cchen@student.hive.fi>
2022-08-11 14:26:39 +0300

    fix typecast bug

--- src/stdlib/ft_uitoa.c
+++ src/stdlib/ft_uitoa.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 10:05:46 by cchen             #+#    #+#             */
-/*   Updated: 2022/02/18 11:15:17 by cchen            ###   ########.fr       */
+/*   Updated: 2022/08/11 14:22:41 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 
 char	*ft_uitoa(unsigned int value, int base, int uppercase)
 {
+	uintmax_t	new_value;
+
 	if (base < 2 || base > 16)
 		return (NULL);
-	return (ft_ultoa((uintmax_t)value, base, uppercase));
+	new_value = value;
+	return (ft_ultoa(new_value, base, uppercase));
 }
