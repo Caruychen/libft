f4b412b7c93403e50638cbf5607285cf0222313b
Caruy Chen <cchen@student.hive.fi>
2022-09-26 16:01:07 +0300

    added size safety in string_from_n

--- src/Strings/string_from_n.c
+++ src/Strings/string_from_n.c
@@ -6,11 +6,12 @@
 /*   By: caruychen <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 14:21:24 by caruychen         #+#    #+#             */
-/*   Updated: 2022/09/25 16:14:24 by caruychen        ###   ########.fr       */
+/*   Updated: 2022/09/26 16:00:40 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Strings.h"
+#include "ft_math.h"
 
 t_string	*string_from_n(t_string *dst, const char *src, size_t len)
 {
@@ -19,7 +20,7 @@ t_string	*string_from_n(t_string *dst, const char *src, size_t len)
 	string_new(dst, len);
 	if (dst->capacity == 0)
 		return (NULL);
-	dst->length = len;
-	dst->memory = ft_memcpy(dst->memory, src, len);
+	dst->length = ft_ulmin(len, ft_strlen(src));
+	dst->memory = ft_memcpy(dst->memory, src, dst->length);
 	return (dst);
 }
